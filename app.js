a846959c2b9929cf0a5fda3bb6cf6a6a7785c85e
Kuchais <tovarvillajuanbenjamin149@gmail.com>
2025-09-17 00:01:01 -0600

    Acomodo de comentarios de forma legible

--- app.js
+++ app.js
@@ -1,37 +1,26 @@
-//Lista para guardar los amigos JS
-let listaAmigos = [];
+let listaAmigos = []; //Lista para guardar los amigos JS
 
-//Referenciar la lista HTML
+let listaHTML = document.getElementById('listaAmigos'); //Referenciar la lista HTML
 //Nota: las listas HTML no tienen propiedad .value
-let listaHTML = document.getElementById('listaAmigos');
 
-//Funcion principal al presionar el boton agregar amigo
-function agregarAmigo(){
-    //Guardar el dato del input en la variable
-    nombreAmigo = document.getElementById('amigo').value;
-    //Si no se ha ingresado un nombre, lanzar alerta
-    if (nombreAmigo == ''){
-        alert("Por favor, inserte un nombre")
+function agregarAmigo(){ //Funcion principal al presionar el boton agregar amigo
+    nombreAmigo = document.getElementById('amigo').value; //Guardar el dato del input en la variable
+    if (nombreAmigo == ''){ //Si no se ha ingresado un nombre
+        alert("Por favor, inserte un nombre") //Lanzar alerta
     }
-    //Si ha puesto un nombre hace lo siguiente
-    else{
-        //En la lista JS agrega el nombre del amigo
-        listaAmigos.push(nombreAmigo);
-        console.log(listaAmigos);
+    else{ //Si ha puesto un nombre
+        listaAmigos.push(nombreAmigo); //En la lista JS agrega el nombre del amigo
+        listaHTML.innerHTML = ""; //Limpiamos la lista para que no se repita al agregar uno nuevo
+        
         /*Para cada amigo que haya en la lista JS, agregamos
         el amigo en la lista HTML
         */
-       //Limpiamos la lista para que no se repita al agregar uno nuevo
-        listaHTML.innerHTML = "";
+        
         for (let amigo of listaAmigos){
-            //Primero creamos un elemento de tipo li (Lista)
-            let li = document.createElement('li');
-            //El contenido de la lista va a ser el nombre del amigo
-            li.textContent = amigo;
-            //Finalmente agregamos el tipo li a la lista HTML
-            listaHTML.appendChild(li);
+            let li = document.createElement('li'); //Primero creamos un elemento de tipo li (Lista)
+            li.textContent = amigo; //El contenido de la lista va a ser el nombre del amigo
+            listaHTML.appendChild(li); //Finalmente agregamos el tipo li a la lista HTML
         }
     }
-    //Al acabar, limpiamos el input
-    document.getElementById('amigo').value = ('');
+    document.getElementById('amigo').value = (''); //Al acabar, limpiamos el input
 }
