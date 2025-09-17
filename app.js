ce77969fe1697ebd0d0ae1e2101407ee0214e128
Kuchais <tovarvillajuanbenjamin149@gmail.com>
2025-09-16 21:45:23 -0600

    Agregar amigos a lista, limpiar input, alerta si no se ha ingresado un nombre

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 let listaAmigos = [];
+
+function agregarAmigo(){
+    nombreAmigo = document.getElementById('amigo').value;
+    if (nombreAmigo == ''){
+        alert("Por favor, inserte un nombre")
+    }
+    else{
+        listaAmigos.push(nombreAmigo);
+    }
+    document.getElementById('amigo').value = ('');
+}
