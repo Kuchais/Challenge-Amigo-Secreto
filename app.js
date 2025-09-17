9d7d1493742ef5d892841f0693c2cfde6407fbb0
Kuchais <tovarvillajuanbenjamin149@gmail.com>
2025-09-17 00:47:02 -0600

    Seleccionamos un indice de la lista de amigos aleatorio y mostramos el amigo secreto en el resultado de HTML

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let listaAmigos = []; //Lista para guardar los amigos JS
 let listaHTML = document.getElementById('listaAmigos'); //Referenciar la lista HTML
 //Nota: las listas HTML no tienen propiedad .value
 
+let resultadoAmigoSecreto = document.getElementById('resultado'); //Referenciar caja del resultado
+
 function agregarAmigo(){ //Funcion principal al presionar el boton agregar amigo
     nombreAmigo = document.getElementById('amigo').value; //Guardar el dato del input en la variable
     if (nombreAmigo == ''){ //Si no se ha ingresado un nombre
@@ -25,8 +27,16 @@ function agregarAmigo(){ //Funcion principal al presionar el boton agregar amigo
     document.getElementById('amigo').value = (''); //Al acabar, limpiamos el input
 }
 
-function sortearAmigo(){
-    if (listaAmigos == ''){
-        alert("Agregue al menos 2 amigos");
+function sortearAmigo(){ //Funcion que se llama cuando se da al boton sortear
+    if (listaAmigos == ''){ 
+        alert("Agregue al menos 2 amigos"); //Alerta de agregar al menos 2 amigos
     }
+    let amigoSecreto = listaAmigos[Math.floor(Math.random()*listaAmigos.length)];
+
+    /*
+    Seleccionamos de manera aleatoria un indice, en este caso no sumamos 1 porque el elemento
+    1 tiene el indice 0
+    */
+
+    resultadoAmigoSecreto.innerHTML = (`Tu amigo secreto es ${amigoSecreto}`); //Mostramos en el resultado el amigo secreto
 }
